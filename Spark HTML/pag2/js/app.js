09df9abe0ee58a9d57737415244fb288b4dd7516
Enzo <villarrealenzohernan@gmail.com>
2018-12-19 21:29:19 -0300

    smooth

--- Spark HTML/pag2/js/app.js
+++ Spark HTML/pag2/js/app.js
@@ -1,37 +1,18 @@
 $(document).foundation();
 
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
+
+//Hace smooth scroll hasta la seccion
+
+$('header a[href^="#"]').on('click',function (e) {
+  // e.preventDefault();
+  
+  altoMenu=$("#header").height()
+  var target = this.hash,
+  $target = $(target);
+
+ $('html, body').stop().animate({
+   'scrollTop': $target.offset().top-altoMenu
+  }, 900, 'swing', function () {
+   window.location.hash = target;
   });
+});
